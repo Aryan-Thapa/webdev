4d719a34b8ac230f912708219720b5371081e163
Aryan-Thapa <aryanthapa219@gmail.com>
2024-09-27 23:11:33 +0530

    async js project

---
+++ chai or JS/lecture-25/bgColorChanger.js
@@ -0,0 +1,22 @@
+const randomValue = () => {
+  return Math.floor(Math.random() * 16);
+};
+const randomColor = () => {
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[randomValue()];
+  }
+  return color;
+};
+
+let colorChanger;
+document.getElementById("startBtn").addEventListener("click", () => {
+  colorChanger = setInterval(() => {
+    document.body.style.backgroundColor = randomColor();
+  }, 1000);
+});
+
+document.getElementById("stopBtn").addEventListener("click", () => {
+  clearTimeout(colorChanger);
+});
